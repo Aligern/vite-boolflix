b284d1954742007dec19c8e2d8ebf8dcf0ab0f2f
Aligern <sasso.luca93@gmail.com>
2024-04-17 12:03:36 +0200

    add store.js

---
+++ src/store.js
@@ -0,0 +1,20 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    // this is our api url
+    apiUrl: 'https://api.themoviedb.org/3/',
+    // these are our api endpoints:
+    endPoint: {
+        movie: 'search/movie',
+        tv: 'search/tv'
+    },
+    options: {
+        params: {
+            // this is our personal API key:
+            api_key: '72b9f59bb742a327478b7837ff8f5382',
+            query: '',
+        }
+    },
+    // this is our image url base path
+    ImageUrl: 'http://image.tmdb.org/t/p/original',
+});
