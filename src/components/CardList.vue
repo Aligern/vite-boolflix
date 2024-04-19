f97bc46203c809eed4f04e75c7c9b78e0fc5a47a
Aligern <sasso.luca93@gmail.com>
2024-04-19 17:19:27 +0200

    add scroll btns

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,5 +1,6 @@
 <template>
 <div id="cardList" >
+    
     <!-- this is our film container -->
     <div id="movieCards" class="container pt-5 pb-5" v-if="store.movie.length > 0">
             <div>
@@ -23,68 +24,71 @@
                 </button>
             </div>
     </div>
+
     <!-- this is our tvseries container -->
-    <div ref="tvScrollContainer" id="tvCards" class="container pt-5 pb-5" v-if="store.movie.length > 0">
+    <div id="tvCards" class="container pt-5 pb-5" v-if="store.movie.length > 0">
             <div>
                 <h2>
                     Serie TV
                 </h2>
             </div>
-        <div class="dflex">
+        <div ref="tvScrollContainer" class="dflex">
             <div class="ls-col" v-for="series in store.tv" :key="series.id">
                 <CardComponent :img="store.ImageUrl + series.poster_path" :name="series.name" :originalName="series.original_name" :language="series.original_language" :vote="series.vote_average" :overview="series.overview"/>
             </div>
         </div>
 
         <div class="d-flex justify-content-between">
-            <button id="scrollLeftTv" @click="scroll(-450, 'tvScrollContainer')">
+            <button id="scrollLeftTv" @click="scroll(-600, 'tvScrollContainer')">
                 scroll left
             </button>
 
-            <button id="scrollRightTv" @click="scroll(450, 'tvScrollContainer')">
+            <button id="scrollRightTv" @click="scroll(600, 'tvScrollContainer')">
                 scroll right
             </button>
         </div>
-
     </div>
+
     <!-- this is our popular movies container -->
-    <div ref="popMovieScrollContainer" id="popularMovies" class="container pt-5 pb-5"  v-if="store.movie.length < 1">
+    <div id="popularMovies" class="container pt-5 pb-5"  v-if="store.movie.length < 1">
             <div>
                 <h2>
                     Film Popolari
                 </h2>
             </div>
-        <div class="dflex">
+        <div ref="popMovieScrollContainer" class="dflex">
             <div class="ls-col" v-for="popularMovie in store.popularMovies" :key="popularMovie.id">
                 <CardComponent :img="store.ImageUrl + popularMovie.poster_path" :name="popularMovie.title" :originalName="popularMovie.original_title" :language="popularMovie.original_language" :vote="popularMovie.vote_average" :overview="popularMovie.overview" />
             </div>
         </div>
         <div class="d-flex justify-content-between">
-            <button id="scrollLeftPopMovie" @click="scroll(-450, 'popMovieScrollContainer')">
+            <button id="scrollLeftPopMovie" @click="scroll(-600, 'popMovieScrollContainer')">
                 scroll left
             </button>
-            <button id="scrollRightPopMovie" @click="scroll(450, 'popMovieScrollContainer')">
+            <button id="scrollRightPopMovie" @click="scroll(600, 'popMovieScrollContainer')">
                 scroll right
             </button>
         </div>
     </div>
+
+
     <!-- this is our popular tv series container -->
-    <div ref="popTvScrollContainer" id="popularTv" class="container pt-5 pb-5"  v-if="store.movie.length < 1">
+    <div id="popularTv" class="container pt-5 pb-5"  v-if="store.movie.length < 1">
             <div>
                 <h2>
                     Serie TV Popolari
                 </h2>
             </div>
-        <div class="dflex">
+        <div ref="popTvScrollContainer" class="dflex">
             <div class="ls-col" v-for="popularTv in store.popularTvs" :key="popularTv.id">
                 <CardComponent :img="store.ImageUrl + popularTv.poster_path" :name="popularTv.name" :originalName="popularTv.original_name" :language="popularTv.original_language" :vote="popularTv.vote_average" :overview="popularTv.overview" />
             </div>
         </div>    
             <div class="d-flex justify-content-between">
-                    <button id="scrollLeftPopTv" @click="scroll(-450, 'popTvScrollContainer')">
+                    <button id="scrollLeftPopTv" @click="scroll(-600, 'popTvScrollContainer')">
                         scroll left
                     </button>
-                    <button id="scrollRightPopTv" @click="scroll(450, 'popTvScrollContainer')">
+                    <button id="scrollRightPopTv" @click="scroll(600, 'popTvScrollContainer')">
                         scroll right
                     </button>
             </div>
@@ -107,9 +111,12 @@ import CardComponent from './CardComponent.vue';
         },
         methods: {
            scroll(distance, id) {
+            console.log('entrato'),
+            console.log(distance),
+            console.log(id),
             this.$refs[id].scrollBy({
                 left: distance,
-                behavior: 'smooth'
+                behavior: 'smooth',
             })
            }
         }
@@ -131,6 +138,4 @@ import CardComponent from './CardComponent.vue';
 }
 
 }
-
-
 </style>
